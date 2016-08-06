2cb77ed9219689d67324d64596916959f518484c
Luis Chávez <luischa04gmail.com>
2016-08-05 19:12:47 -0600

    Facebook-Api half implemented

--- application/controllers/Login_controller.php
+++ application/controllers/Login_controller.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+include("./vendor/autoload.php");
 
 class Login_controller extends CI_Controller
 {
@@ -14,6 +15,15 @@ class Login_controller extends CI_Controller
   {
     $data['titulo']= 'Login';
     $data['token'] = $this->token();
+
+    $fb = new Facebook\Facebook([
+      'app_id' => '1092965707405902',
+      'app_secret' => 'f053d3491233211c2266ed52748c1b2c',
+      'default_graph_version' => 'v2.7',
+    ]);
+    $helper = $fb->getRedirectLoginHelper();
+    $permissions = ['email', 'user_posts'];
+    $data['loginUrl'] = $helper->getLoginUrl(base_url().'Login_controller/fb_login');
     $this->load->view('Login_view',$data);
   }
 
@@ -31,13 +41,48 @@ class Login_controller extends CI_Controller
     return $token;
   }
 
+  public function fb_login()
+  {
+    $fb = new Facebook\Facebook([
+      'app_id' => '1092965707405902',
+      'app_secret' => 'f053d3491233211c2266ed52748c1b2c',
+      'default_graph_version' => 'v2.7',
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+    try {
+      $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+      echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // When validation fails or other local issues
+      echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
+    }
+
+    if (isset($accessToken)) {
+      $fb->setDefaultAccessToken($accessToken);
+      try {
+        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+        $profile = $profile_request->getGraphNode()->asArray();
+
+        print_r($profile['id']);
+      } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error:v ' . $e->getMessage();
+      }
+    }
+  }
+
   public function entrada_login()
   {
     // Verify the token
     if ($this->input->post('token') && $this->input->post('token') == $this->session->userdata('token'))
     {
 
-        // Validation rules
+      // Validation rules
 
       $this->form_validation->set_rules('email', 'email', 'required|trim|valid_email|max_length[50]|htmlspecialchars');
       $this->form_validation->set_rules('password', 'password', 'required|trim|max_length[50]|htmlspecialchars');
@@ -63,15 +108,15 @@ class Login_controller extends CI_Controller
             'is_logued_in' => TRUE,
             'username' => $login->username,
             'email' => $login->email);
-          $this->session->set_userdata($dat);
+            $this->session->set_userdata($dat);
 
-          $url = base_url().'Home';
-          echo "<script> alert('Welcome');
-          window.location.href='$url';
-          </script>";
-        }
-        else
-        {
+            $url = base_url().'Home';
+            echo "<script> alert('Welcome');
+            window.location.href='$url';
+            </script>";
+          }
+          else
+          {
 
             $url = base_url().'Login_controller';
             echo "<script> alert('That user does not exist');
@@ -83,60 +128,60 @@ class Login_controller extends CI_Controller
     }
 
 
-      public function logout()
-      {
-        $this->session->sess_destroy();
-        $url = base_url().'Home';
-        echo "<script> alert('¡See ya!');
-        window.location.href = '$url'; </script>";
+    public function logout()
+    {
+      $this->session->sess_destroy();
+      $url = base_url().'Home';
+      echo "<script> alert('¡See ya!');
+      window.location.href = '$url'; </script>";
     }
 
 
-  public function registro_user()
-  {
-
-    if($this->input->post('token') && $this->input->post('token') == $this->session->userdata('token'))
+    public function registro_user()
     {
 
-		  // Validation rules
-     $this->form_validation->set_rules('email', 'email', 'required|valid_email|trim|max_length[40]|htmlspecialchars');
-     $this->form_validation->set_rules('username', 'username', 'required|trim|max_length[20]|htmlspecialchars');
-     $this->form_validation->set_rules('password', 'password', 'required|trim|min_length[8]|max_length[20]|htmlspecialchars');
+      if($this->input->post('token') && $this->input->post('token') == $this->session->userdata('token'))
+      {
 
-      // Error messages
-			//$this->form_validation->set_message('alpha_spaces', 'The field only accept alphabetic characters');
-     $this->form_validation->set_message('valid_email', '*Invalid email');
-     $this->form_validation->set_message('required', '*Required field');
-     $this->form_validation->set_message('min_length', '*The field %s must be at least %s characters');
-     $this->form_validation->set_message('max_length', '*The field %s cant be more than %s characters');
+        // Validation rules
+        $this->form_validation->set_rules('email', 'email', 'required|valid_email|trim|max_length[40]|htmlspecialchars');
+        $this->form_validation->set_rules('username', 'username', 'required|trim|max_length[20]|htmlspecialchars');
+        $this->form_validation->set_rules('password', 'password', 'required|trim|min_length[8]|max_length[20]|htmlspecialchars');
 
-     if (!$this->form_validation->run())
-     {
-        //Si no pasamos la validación volvemos al formulario mostrando los errores
+        // Error messages
+        //$this->form_validation->set_message('alpha_spaces', 'The field only accept alphabetic characters');
+        $this->form_validation->set_message('valid_email', '*Invalid email');
+        $this->form_validation->set_message('required', '*Required field');
+        $this->form_validation->set_message('min_length', '*The field %s must be at least %s characters');
+        $this->form_validation->set_message('max_length', '*The field %s cant be more than %s characters');
+
+        if (!$this->form_validation->run())
+        {
+          //Si no pasamos la validación volvemos al formulario mostrando los errores
           $this->index_registro();
-       }
-       else
-       {
+        }
+        else
+        {
           //Si funciona se procesan los datos
           $email = $this->input->post('email');
           $username = $this->input->post('username');
           $password = $this->input->post('password');
           $hash = $this->bcrypt->hash_password($password);
-      		//Probar si la contraseña se encripto
-      if ($this->bcrypt->check_password($password, $hash))
-      {
-        $insert_pass = $this->Login_model->registro($username, $email, $hash);
-        if ($insert_pass)
-        {
-          $url = base_url().'Login_controller/index';
-          echo "<script> alert ('¡Saved!');
-          window.location.href = '$url';
-          </script>";
-        }
-        else
+          //Probar si la contraseña se encripto
+          if ($this->bcrypt->check_password($password, $hash))
+          {
+            $insert_pass = $this->Login_model->registro($username, $email, $hash);
+            if ($insert_pass)
             {
-          throw new Exception("Error Processing request", 1);
-          echo Exception;
+              $url = base_url().'Login_controller/index';
+              echo "<script> alert ('¡Saved!');
+              window.location.href = '$url';
+              </script>";
+            }
+            else
+            {
+              throw new Exception("Error Processing request", 1);
+              echo Exception;
             }
           }
         }
--- application/views/Login_view.php
+++ application/views/Login_view.php
@@ -8,44 +8,45 @@
 	<link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url()?>assets/css/login.css" rel="stylesheet">
 </head>
-	<body>
+<body>
 
-			<!--Pulling Awesome Font -->
-			<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<!--Pulling Awesome Font -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
-			<div class="container">
-				<div class="row">
-					<div>
-						<div class="text">
-							<h4>Please login</h4>
-						</div>
-						<div class="form-login">
-
-						<form  action="<?php echo base_url() ?>Login_controller/entrada_login" method="post">
-							<p>Email:</p>
-
-					<input type="text" name="email" class="form-control input-sm chat-input"/>
-							<span class="text-danger"><?php echo form_error('email'); ?></span>
-
-
-							<br>
-
-							<p>Password:</p>
-							<input type="password" name="password" class="form-control input-sm chat-input" />
-							<span class="text-danger"><?php echo form_error('password')?></span>
-
-							</br>
-							<input type="hidden" name="token" value="<?php echo $token?>" />
-							<div class="wrapper">
-								<input class="button" type="submit" name="submit" value="Login">
-							</div>
-
-						</form>
-
-						</div>
-					</div>
+	<div class="container">
+		<div class="row">
+			<div>
+				<div class="text">
+					<h4>Please login</h4>
 				</div>
-			</div>
+				<div class="form-login">
 
-		</body>
-	</html>
+					<form  action="<?php echo base_url() ?>Login_controller/entrada_login" method="post">
+						<p>Email:</p>
+
+						<input type="text" name="email" class="form-control input-sm chat-input"/>
+						<span class="text-danger"><?php echo form_error('email'); ?></span>
+
+
+						<br>
+
+						<p>Password:</p>
+						<input type="password" name="password" class="form-control input-sm chat-input" />
+						<span class="text-danger"><?php echo form_error('password')?></span>
+
+					</br>
+					<input type="hidden" name="token" value="<?php echo $token?>" />
+					<div class="wrapper">
+						<input class="button" type="submit" name="submit" value="Login">
+					</div>
+
+				</form>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<a href="<?php echo $loginUrl?>">Log in with Facebook!</a>
+</body>
+</html>
