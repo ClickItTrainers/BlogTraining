366725c4b35d95a1f3e5db626eb1dd226b388631
cesar valdez <cesar.valdez@gmail.com>
2016-08-07 04:17:26 -0500

    added new design of the login view

--- application/views/Login_view.php
+++ application/views/Login_view.php
@@ -9,22 +9,21 @@
 
 	<link href="<?php echo base_url()?>assets/css/login.css" rel="stylesheet">
 </head>
-	<body>
+	<body class="img-fluid">
 
 			<!--Pulling Awesome Font -->
 			<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-12 col-md-8 col-sm-12 col-xs-12">
+				<div class="col-lg-4 col-md-8 col-sm-12 col-xs-12">
 
-						<div class="text">
-							<h4>Please login</h4>
-						</div>
+						
 						<div class="form-login">
 						<form  action="<?php echo base_url() ?>Login_controller/entrada_login" method="post">
+							<h4>Please login</h4>
 							<p>Email:</p>
-					<input type="text" name="email" class="form-control input-sm chat-input"/>
+					<input type="text" name="email" class=" col-center form-control input-sm chat-input"/>
 							<span class="text-danger"><?php echo form_error('email'); ?></span>
 							<br>
 							<p>Password:</p>
@@ -34,11 +33,13 @@
 							<input type="hidden" name="token" value="<?php echo $token?>" />
 							
 							<div class="wrapper">
-								<input class=" button btn azm-social azm-btn azm-border-bottom azm-drupal" type="submit" name="submit" value="Login">
+								<div class="display">
+									<input class=" button btn azm-social azm-btn azm-border-bottom azm-drupal" type="submit" name="submit" value="Login">
+								</div>
 								<!-- <a href="#" class="btn azm-social azm-btn azm-border-bottom azm-drupal"><i class="fa"></i> Login </a> -->
 
 								<a href="#" class="btn azm-social azm-btn azm-border-bottom azm-facebook"><i class="fa fa-facebook"></i></a>
-								<a href="#" class="btn azm-social azm-btn azm-border-bottom azm-twitter"><i class="fa fa-twitter"></i> </a>
+								<a href="#" class="btn azm-social azm-btn azm-border-bottom azm-twitter"><i class="fa fa-twitter"></i></a>
 								<a href="#" class="btn azm-social azm-btn azm-border-bottom azm-google-plus"><i class="fa fa-google"></i></a>
 							</div>
 						</form>
