fddfd758e11bd1581d263cb017f09b86d08a1dbb
Mena <menagrunge@gmail.com>
2016-08-12 17:51:53 -0500

    Removed comments on config file

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-//$config['base_url'] = 'http://149.56.23.33:3080/';
-$config['base_url'] = 'http://www.blogtraining.com/';
+$config['base_url'] = 'http://149.56.23.33:3080/';
+//$config['base_url'] = 'http://www.blogtraining.com/';
 $config['index_page'] = '';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '';
