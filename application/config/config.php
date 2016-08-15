3fa1f89595e3a35e0323b18c4a326aaf60703a07
TonySaanchez <activatetony@gmail.com>
2016-08-15 14:47:51 -0500

    the images were added for the posts and the category name

--- application/config/config.php
+++ application/config/config.php
@@ -5,6 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'http://149.56.23.33:3080/';
 // $config['base_url'] = 'http://www.blogtraining.com/';
 
+
+
 $config['index_page'] = '';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '';
