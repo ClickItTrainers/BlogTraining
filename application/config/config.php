229dfd768a6afbeb813b2701dca2ab2e27ffb54c
Chavez04 <luischa04@gmail.com>
2016-08-29 08:39:06 -0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -3,11 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-//$config['base_url'] = 'http://unblogtraining.tk/';
+$config['base_url'] = 'http://unblogtraining.tk/';
 
 //$config['base_url'] = 'http://149.56.23.33:3080/';
 
-$config['base_url'] = 'http://www.blogtraining.com/';
+//$config['base_url'] = 'http://www.blogtraining.com/';
 
 
 
